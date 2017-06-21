d766991476de725df4712721edf441db5f376ac1
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-21 19:40:12 -0400

    Made initial display setup.

--- display_client/js/display.js
+++ display_client/js/display.js
@@ -1,14 +1,13 @@
 $(function() {
   function setupPlayerList(player_list, client) {
 
-    var html = "";
+    var html = "<h1>Select player ID:</h1>";
     for(var i = 0; i < player_list.length; i++) {
       html += '<div><a class="btn btn-default choose-player-btn">'+
               player_list[i]+
               '</a></div>';
     }
 
-    $('#status')[0].innerHTML = "Select your player ID:";
     $('#content')[0].innerHTML = html;
 
     $('.choose-player-btn').click(function(e) {
@@ -16,17 +15,12 @@ $(function() {
       var player_id = e.target.innerHTML;
 
       $('#content')[0].innerHTML = "";
-      $('#status')[0].innerHTML = "Waiting for events...";
 
       client.selectPlayer(player_id);
     });
   };
 
   var client = new DisplayClient();
-  client.onAnyChange(function(data) {
-    $('#status')[0].innerHTML = "Received event: "+JSON.stringify(data);
-  });
-
   client.onPlayerList(function(player_list) {
     setupPlayerList(player_list, client);
   });
