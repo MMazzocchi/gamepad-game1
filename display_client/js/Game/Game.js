b633bc0b18fdcf62d03411533789676e218d5df4
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-21 22:21:55 -0400

    Added rotation of cube using analog control stic.

--- display_client/js/Game/Game.js
+++ display_client/js/Game/Game.js
@@ -1,7 +1,8 @@
 var Game = function(renderer, client) {
   console.log("Making a new game!");
-
+  this.client = client;
   this.renderer = renderer;
+
   this.camera = new THREE.PerspectiveCamera(75,
     window.innerWidth / window.innerHeight, 1, 10000);
   this.camera.position.z = 1000;
@@ -22,7 +23,19 @@ var Game = function(renderer, client) {
 Game.prototype.render = function() {
   this.renderer.render(this.scene, this.camera);
 
-  this.mesh.rotation.y += 0.01;
+  var dyr = this.client.getState('axis', 0);
+  if(dyr != undefined) {
+    this.mesh.rotateOnAxis(
+      this.mesh.worldToLocal(new THREE.Vector3(0, 1, 0)),
+      dyr*0.5);
+  }
+
+  var dxr = this.client.getState('axis', 1);
+  if(dxr != undefined) {
+    this.mesh.rotateOnAxis(
+      this.mesh.worldToLocal(new THREE.Vector3(1, 0, 0)),
+      dxr*0.5);
+  }
 
   var game = this;
   window.requestAnimationFrame(function() {
