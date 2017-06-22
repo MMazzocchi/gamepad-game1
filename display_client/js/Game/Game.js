e07e5bb506d7395c4b006815f2a55c2a78a73015
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-21 22:03:45 -0400

    Animated, made cube rotate.

--- display_client/js/Game/Game.js
+++ display_client/js/Game/Game.js
@@ -1,9 +1,11 @@
 var Game = function(renderer, client) {
   console.log("Making a new game!");
 
+  this.renderer = renderer;
   this.camera = new THREE.PerspectiveCamera(75,
     window.innerWidth / window.innerHeight, 1, 10000);
   this.camera.position.z = 1000;
+
 //  this.controls = new THREE.DeviceOrientationControls(camera);
   this.scene = new THREE.Scene();
 
@@ -11,10 +13,21 @@ var Game = function(renderer, client) {
   var material = new THREE.MeshBasicMaterial(
     { color: 0xff0000, wireframe: true } );
  
-  var mesh = new THREE.Mesh(geometry, material);
-  this.scene.add(mesh);
+  this.mesh = new THREE.Mesh(geometry, material);
+  this.scene.add(this.mesh);
 
-  renderer.render(this.scene, this.camera);
+  this.render();
+};
+
+Game.prototype.render = function() {
+  this.renderer.render(this.scene, this.camera);
+
+  this.mesh.rotation.y += 0.01;
+
+  var game = this;
+  window.requestAnimationFrame(function() {
+    game.render();
+  });
 };
 
 Game.prototype.setSize = function(width, height) {
